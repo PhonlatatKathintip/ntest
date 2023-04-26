0b729a9c5305a9399e3631d1f356ca06ab348348
Phonlatat.work <Phonlatues.w@gmail.com>
2023-04-26 16:19:37 +0700

    mute-image

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function index() {
     <div>
       <div className="d-flex flex-row mb-3 justify-center">
         <Card>
-          <img src="https://th.bing.com/th/id/OIP.buvvTQK0cFvJwLZYhCjjGAAAAA?w=302&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7"></img>
+          {/* <img src="https://th.bing.com/th/id/OIP.buvvTQK0cFvJwLZYhCjjGAAAAA?w=302&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7"></img> */}
           <text className="h1">Nike Air Jordan</text>
         </Card>
       </div>
