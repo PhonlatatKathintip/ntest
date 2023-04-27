70fbf2784502bb47adaac01199c75586e23ed0d0
Phonlatat.work <Phonlatues.w@gmail.com>
2023-04-27 19:08:53 +0700

    4/27/23

---
+++ pages/data/products.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function products() {
+  return (
+    {
+        slug: 1,
+        image: {
+          url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+        },
+        name: "Jordan 1 Retro High OG Chicago",
+        price: 200,
+      },
+      {
+        slug: 2,
+        image: {
+          url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+        },
+        name: "Jordan 1 Retro High OG Chicago",
+        price: 200,
+      },
+      {
+        slug: 3,
+        image: {
+          url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+        },
+        name: "Jordan 1 Retro High OG Chicago",
+        price: 200,
+      },
+      {
+        slug: 3,
+        image: {
+          url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+        },
+        name: "Jordan 1 Retro High OG Chicago",
+        price: 200,
+      },
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card } from "@material-tailwind/react";
 import Box from "@mui/material/Box";
+import Link from "next/link";
+import { Button } from "@mui/material";
 
 const commonStyles = {
   bgcolor: "background.paper",
@@ -10,19 +12,79 @@ const commonStyles = {
   height: "5rem",
 };
 
-export default function index() {
+export default function Home() {
+  const allProducts = [
+    {
+      slug: 1,
+      image: {
+        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+      },
+      name: "Jordan 1 Retro High OG Chicago",
+      price: 200,
+    },
+    {
+      slug: 2,
+      image: {
+        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+      },
+      name: "Jordan 1 Retro High OG Chicago",
+      price: 200,
+    },
+    {
+      slug: 3,
+      image: {
+        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+      },
+      name: "Jordan 1 Retro High OG Chicago",
+      price: 200,
+    },
+    {
+      slug: 3,
+      image: {
+        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+      },
+      name: "Jordan 1 Retro High OG Chicago",
+      price: 200,
+    },
+  ];
+
   return (
-    <div>
-      <div className="d-flex flex-row mb-3 justify-center">
-        <Card>
-          <Box sx={{ commonStyles, borderColor: "grey.500" }}>
-            <div className="">
-              <img src="https://th.bing.com/th/id/OIP.buvvTQK0cFvJwLZYhCjjGAAAAA?w=302&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7"></img>
-              <text className="h1">Nike Air Jordan</text>
-            </div>
-          </Box>
-        </Card>
-      </div>
+    <div className="flex-1 flex-row grid grid-cols-4 gap-4">
+      {allProducts.map((product) => {
+        return (
+          <div>
+            <Card>
+              <div key={product.id}>
+                <Link href={`products/${product.slug}`}>
+                  <div>
+                    <img
+                      height={380}
+                      width={380}
+                      src={product.image.url}
+                      alt={product.name}
+                    />
+                  </div>
+                </Link>
+                <div className=" rounded-xl text-center bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
+                  <div>
+                    <h3>{product.name}</h3>
+                    <p>${product.price}</p>
+                  </div>
+                  <Button>Add to cart 🛒</Button>
+                </div>
+              </div>
+            </Card>
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       allProducts,
+//     },
+//   };
+// }
