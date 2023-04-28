52deb4c92b64ac4b11ebe1a50aa2b3d733870df9
Phonlatat.work <Phonlatues.w@gmail.com>
2023-04-28 13:55:12 +0700

    main-ok-but-not-good

--- pages/index.js
+++ pages/index.js
@@ -3,101 +3,57 @@ import { Card } from "@material-tailwind/react";
 import Box from "@mui/material/Box";
 import Link from "next/link";
 import { Button } from "@mui/material";
-//import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import products from "./data/products";
 
-// export async function getStaticProps() {
-//   const client = new ApolloClient({
-//     uri: "https://api-ap-northeast-1.hygraph.com/v2/clgyx5z41246101ue9mnm4lf8/master",
-//     cache: new InMemoryCache(),
-//   });
-//   const data = await client.query({
-//     query: gql`
-//       query ProductsQuery {
-//         products {
-//           id
-//           name
-//           slug
-//           price
-//           image {
-//             url
-//           }
-//         }
-//       }
-//     `,
-//   });
-//   const allProducts = data.data.products;
-//   return {
-//     props: {
-//       allProducts,
-//     },
-//   };
-// }
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://api-ap-northeast-1.hygraph.com/v2/clgyx5z41246101ue9mnm4lf8/master",
+    cache: new InMemoryCache(),
+  });
+  const data = await client.query({
+    query: gql`
+      query MyQuery {
+        ntests {
+          id
+          image
+          name
+          price
+          slug
+        }
+      }
+    `,
+  });
+  const ntests = data.data.ntests;
+  return {
+    props: {
+      ntests,
+    },
+  };
+}
 
-const commonStyles = {
-  bgcolor: "background.paper",
-  m: 1,
-  border: 1,
-  width: "5rem",
-  height: "5rem",
-};
-
-export default function Home() {
-  const allProducts = [
-    {
-      slug: 1,
-      image: {
-        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
-      },
-      name: "Jordan 1 Retro High OG Chicago",
-      price: 200,
-    },
-    {
-      slug: 2,
-      image: {
-        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
-      },
-      name: "Jordan 1 Retro High OG Chicago",
-      price: 200,
-    },
-    {
-      slug: 3,
-      image: {
-        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
-      },
-      name: "Jordan 1 Retro High OG Chicago",
-      price: 200,
-    },
-    {
-      slug: 3,
-      image: {
-        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
-      },
-      name: "Jordan 1 Retro High OG Chicago",
-      price: 200,
-    },
-  ];
-
+export default function Home({ ntests }) {
   return (
     <div className="flex-1 flex-row grid grid-cols-4 gap-4 mr-3 ml-3">
-      {allProducts.map((product) => {
+      {ntests.map((ntests) => {
         return (
           <div>
             <Card className="justify-center mb-3 mt-3  flex-wrap">
-              <div key={product.id}>
-                <Link href={`products/${product.slug}`}>
+              <div key={ntests.id}>
+                <Link href={`products/${ntests.slug}`}>
                   <div>
                     <img
                       height={320}
                       width={320}
-                      src={product.image.url}
-                      alt={product.name}
+                      src={ntests.image}
+                      alt={ntests.name}
                     />
                   </div>
                 </Link>
                 <div className=" rounded-xl text-center bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 mb-3">
                   <div>
-                    <h3>{product.name}</h3>
-                    <p>${product.price}</p>
+                    <h3>{ntests.name}</h3>
+                    <p>${ntests.price}</p>
                   </div>
                   <Button>Add to cart 🛒</Button>
                 </div>
@@ -109,11 +65,3 @@ export default function Home() {
     </div>
   );
 }
-
-// export async function getStaticProps() {
-//   return {
-//     props: {
-//       allProducts,
-//     },
-//   };
-// }
--- pages/products/[productslug].js
+++ pages/products/[productslug].js
@@ -1,30 +1,92 @@
 import Head from "next/head";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import Image from "next/image";
+import React from "react";
 
-const singleproduct = () => {
+const singleproduct = ({ ntests }) => {
   return (
-    <>
-      <Head>
-        <title>Dracaena fragrans</title>
-      </Head>
+    <div>
       <div>
-        <div>
-          <img src="/images/croton.png" alt="" />
-        </div>
-        <div>
-          <h3>Dracaena fragrans</h3>
-          <p>$50</p>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. At
-              impedit voluptatum vitae labore molestiae, maiores, hic ad
-              officiis laudantium in officia, nam vel quod! Nesciunt aperiam
-              explicabo facere laboriosam eius.
-            </p>
-          </div>
-          <button className="btn">Add to cart 🛒</button>
-        </div>
+        <Image src={ntests.image} width={300} height={700} alt="" />
       </div>
-    </>
+      <div>
+        <h3>{ntest.name}</h3>
+        <p className="price">${ntests.price}</p>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At impedit
+            voluptatum vitae labore molestiae, maiores, hic ad officiis
+            laudantium in officia, nam vel quod! Nesciunt aperiam explicabo
+            facere laboriosam eius.
+          </p>
+        </div>
+        <button className="">Add to cart 🛒</button>
+      </div>
+    </div>
   );
 };
 export default singleproduct;
+
+export async function getStaticPaths({ params }) {
+  const client = new ApolloClient({
+    uri: "https://api-ap-northeast-1.hygraph.com/v2/clgyx5z41246101ue9mnm4lf8/master",
+    cache: new InMemoryCache(),
+  });
+  const data = await client.query({
+    query: gql`
+      query MyQuery {
+        ntests {
+          id
+          name
+          slug
+          price
+          image
+        }
+      }
+    `,
+  });
+
+  const paths = data.data.ntests.map((singleProduct) => {
+    return {
+      params: {
+        ntestslug: singleProduct.slug,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const client = new ApolloClient({
+    uri: "https://api-ap-northeast-1.hygraph.com/v2/clgyx5z41246101ue9mnm4lf8/master",
+    cache: new InMemoryCache(),
+  });
+
+  const data = await client.query({
+    query: gql`
+      query MyQuery($slug: String) {
+        ntests(where: { slug: $slug }) {
+          id
+          name
+          price
+          slug
+          image
+        }
+      }
+    `,
+    variables: {
+      slug: params.ntestslug,
+    },
+  });
+
+  const ntests = data.data.ntests;
+  return {
+    props: {
+      ntests,
+    },
+  };
+}
