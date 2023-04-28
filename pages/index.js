b31d19e2655a98d97d56e310c9b234171ef2454f
Phonlatat.work <Phonlatues.w@gmail.com>
2023-04-28 08:53:02 +0700

    on-site

--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,35 @@ import { Card } from "@material-tailwind/react";
 import Box from "@mui/material/Box";
 import Link from "next/link";
 import { Button } from "@mui/material";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+//import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-export async function getStaticProps() {
-  const client = new ApolloClient({
-    uri: "https://api-ap-northeast-1.hygraph.com/v2/clgyx5z41246101ue9mnm4lf8/master",
-    cache: new InMemoryCache(),
-  });
-  const data = await client.query({
-    query: gql`
-      query ProductsQuery {
-        products {
-          id
-          name
-          slug
-          price
-          image {
-            url
-          }
-        }
-      }
-    `,
-  });
-  const allProducts = data.data.products;
-  return {
-    props: {
-      allProducts,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const client = new ApolloClient({
+//     uri: "https://api-ap-northeast-1.hygraph.com/v2/clgyx5z41246101ue9mnm4lf8/master",
+//     cache: new InMemoryCache(),
+//   });
+//   const data = await client.query({
+//     query: gql`
+//       query ProductsQuery {
+//         products {
+//           id
+//           name
+//           slug
+//           price
+//           image {
+//             url
+//           }
+//         }
+//       }
+//     `,
+//   });
+//   const allProducts = data.data.products;
+//   return {
+//     props: {
+//       allProducts,
+//     },
+//   };
+// }
 
 const commonStyles = {
   bgcolor: "background.paper",
@@ -42,47 +42,47 @@ const commonStyles = {
 };
 
 export default function Home() {
-  // const allProducts = [
-  //   {
-  //     slug: 1,
-  //     image: {
-  //       url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
-  //     },
-  //     name: "Jordan 1 Retro High OG Chicago",
-  //     price: 200,
-  //   },
-  //   {
-  //     slug: 2,
-  //     image: {
-  //       url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
-  //     },
-  //     name: "Jordan 1 Retro High OG Chicago",
-  //     price: 200,
-  //   },
-  //   {
-  //     slug: 3,
-  //     image: {
-  //       url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
-  //     },
-  //     name: "Jordan 1 Retro High OG Chicago",
-  //     price: 200,
-  //   },
-  //   {
-  //     slug: 3,
-  //     image: {
-  //       url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
-  //     },
-  //     name: "Jordan 1 Retro High OG Chicago",
-  //     price: 200,
-  //   },
-  // ];
+  const allProducts = [
+    {
+      slug: 1,
+      image: {
+        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+      },
+      name: "Jordan 1 Retro High OG Chicago",
+      price: 200,
+    },
+    {
+      slug: 2,
+      image: {
+        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+      },
+      name: "Jordan 1 Retro High OG Chicago",
+      price: 200,
+    },
+    {
+      slug: 3,
+      image: {
+        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+      },
+      name: "Jordan 1 Retro High OG Chicago",
+      price: 200,
+    },
+    {
+      slug: 3,
+      image: {
+        url: "https://sasom.co.th/_next/image?url=https%3A%2F%2Fd2cva83hdk3bwc.cloudfront.net%2Fjordan-1-retro-high-og-chicago-1.jpg&w=1920&q=75",
+      },
+      name: "Jordan 1 Retro High OG Chicago",
+      price: 200,
+    },
+  ];
 
   return (
-    <div className="flex-1 flex-row grid grid-cols-4 gap-4">
+    <div className="flex-1 flex-row grid grid-cols-4 gap-4 mr-3 ml-3">
       {allProducts.map((product) => {
         return (
           <div>
-            <Card className="justify-center mb-3 mt-3 flex-wrap">
+            <Card className="justify-center mb-3 mt-3  flex-wrap">
               <div key={product.id}>
                 <Link href={`products/${product.slug}`}>
                   <div>
